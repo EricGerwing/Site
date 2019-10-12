9b3ed18098fc490538c1cddd19f409697eee669f
EricGerwing <ericgerwing@gmail.com>
2019-10-12 13:28:08 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,8 +45,8 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
-                            magna sed accumsan arcu neque.</h2>
+                            <h2>This portflio page is under construction.</h2>
+                            <h2>Last Updated October 2019.</h2>
                         </header>
                         <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
                         <ul className="actions">
