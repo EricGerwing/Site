369050ad7e723566b39f13a629752237946746b2
EricGerwing <ericgerwing@gmail.com>
2019-11-10 09:02:44 -0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,18 @@
 import  React  from  "react"
 import Layout from ".../components/layout"
-
-
 import Helmet from 'react-helmet'
 import {Link} from 'gatsby'
-
-import Layout from '../components/layout'
-// import Lightbox from 'react-images'
+import Layout from '../components/layout' 
+import Lightbox from 'react-images'
 import Gallery from '../components/Gallery'
 
-  function AboutPage(){
-      return (
+  const AboutPage = () =>{
+    <Layout> 
+    return (
           <main>
             <h1>This is the about page</h1>
           <main>
           )
+    </Layout>
   }
     export  default  AboutPage
